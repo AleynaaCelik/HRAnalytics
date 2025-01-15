cae27999920bd6d8e2fe0c2d610251286acba602
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-15 20:11:07 +0300

    AuditableEntity Created

---
+++ HRAnalytics.Core/Common/AuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Core.Common
+{
+    public abstract class AuditableEntity:BaseEntity
+    {
+        public string? Notes { get; set; }
+        public string? Description { get; set; }
+    }
+}
