e8d0c150109b39be9faa60f5e5d87f26b271116e
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-16 16:06:57 +0300

    Infrastructure Extensions Added

---
+++ HRAnalytics.Infrastructure/Extension/InfrastructureExtensions.cs
@@ -0,0 +1,34 @@
+﻿using HRAnalytics.Core.Interfaces;
+using HRAnalytics.Infrastructure.Context;
+using HRAnalytics.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Infrastructure.Extension
+{
+    public static class InfrastructureExtensions
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
+
