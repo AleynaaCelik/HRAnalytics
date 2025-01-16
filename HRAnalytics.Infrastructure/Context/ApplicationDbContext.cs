717dbdd3a8b45ca7c94ea59d75f8b456ff898b00
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-16 15:44:07 +0300

    ApplicationDbContext Added

---
+++ HRAnalytics.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRAnalytics.Core.Entities;
+using HRAnalytics.Core.Common;
+
+namespace HRAnalytics.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<EmployeeProgress> EmployeeProgresses { get; set; }
+        public DbSet<LearningModule> LearningModules { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.LastName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.PhoneNumber).HasMaxLength(20);
+
+                entity.HasOne(e => e.Department)
+                      .WithMany(d => d.Employees)
+                      .HasForeignKey(e => e.DepartmentId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasKey(d => d.Id);
+                entity.Property(d => d.Name).IsRequired().HasMaxLength(100);
+                entity.Property(d => d.Code).HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<EmployeeProgress>(entity =>
+            {
+                entity.HasKey(ep => ep.Id);
+                entity.Property(ep => ep.CompletionPercentage)
+                      .HasPrecision(5, 2);
+
+                entity.HasOne(ep => ep.Employee)
+                      .WithMany(e => e.ProgressRecords)
+                      .HasForeignKey(ep => ep.EmployeeId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(ep => ep.Module)
+                      .WithMany(m => m.ProgressRecords)
+                      .HasForeignKey(ep => ep.ModuleId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<LearningModule>(entity =>
+            {
+                entity.HasKey(m => m.Id);
+                entity.Property(m => m.Name).IsRequired().HasMaxLength(200);
+                entity.Property(m => m.Content).HasMaxLength(4000);
+            });
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        entry.Entity.IsDeleted = false;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedDate = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
+
