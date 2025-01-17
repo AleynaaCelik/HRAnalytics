8497259753cd1ad988ba469cc9c0638a7240175c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-17 17:57:13 +0300

    EmployeeCommand Operations Added

---
+++ HRAnalytics.Application/Features/Employees/Commands/CreateEmployeeCommand .cs
@@ -0,0 +1,20 @@
+﻿using HRAnalytics.Application.DTOs.Employee;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Application.Features.Employees.Commands
+{
+    public class CreateEmployeeCommand : IRequest<EmployeeDto>
+    {
+        public CreateEmployeeDto Employee { get; set; }
+
+        public CreateEmployeeCommand(CreateEmployeeDto employee)
+        {
+            Employee = employee;
+        }
+    }
+}
---
+++ HRAnalytics.Application/Features/Employees/Commands/UpdateEmployeeCommand .cs
@@ -0,0 +1,23 @@
+﻿using HRAnalytics.Application.DTOs.Employee;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Application.Features.Employees.Commands
+{
+    public class UpdateEmployeeCommand : IRequest<EmployeeDto>
+    {
+        public int Id { get; set; }
+        public UpdateEmployeeDto Employee { get; set; }
+
+        public UpdateEmployeeCommand(int id, UpdateEmployeeDto employee)
+        {
+            Id = id;
+            Employee = employee;
+        }
+    }
+
+}
