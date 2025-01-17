c1c3d215540318257aa45ef2681863adb00d9fa0
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-17 17:40:04 +0300

    Progress Dtos Added

---
+++ HRAnalytics.Application/DTOs/Progress/EmployeeProgressDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Application.DTOs.Progress
+{
+    public class EmployeeProgressDto
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; } = default!;
+        public decimal OverallProgress { get; set; }
+        public List<ModuleProgressDto> ModuleProgress { get; set; } = new();
+    }
+}
---
+++ HRAnalytics.Application/DTOs/Progress/ModuleProgressDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Application.DTOs.Progress
+{
+    public class ModuleProgressDto
+    {
+        public int ModuleId { get; set; }
+        public string ModuleName { get; set; } = default!;
+        public decimal CompletionPercentage { get; set; }
+        public string Status { get; set; } = default!;
+        public DateTime? CompletionDate { get; set; }
+    }
+}
