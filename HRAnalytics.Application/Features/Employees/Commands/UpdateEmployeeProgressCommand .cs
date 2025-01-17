94b48ed53eab9400e8f79c826f9f47edf6f5bdf1
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-17 18:01:15 +0300

    UpdateEmployeeProgressCommand Added

---
+++ HRAnalytics.Application/Features/Employees/Commands/UpdateEmployeeProgressCommand .cs
@@ -0,0 +1,24 @@
+﻿using HRAnalytics.Application.DTOs.Progress;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Application.Features.Employees.Commands
+{
+    public class UpdateEmployeeProgressCommand : IRequest<ModuleProgressDto>
+    {
+        public int EmployeeId { get; set; }
+        public int ModuleId { get; set; }
+        public decimal CompletionPercentage { get; set; }
+
+        public UpdateEmployeeProgressCommand(int employeeId, int moduleId, decimal completionPercentage)
+        {
+            EmployeeId = employeeId;
+            ModuleId = moduleId;
+            CompletionPercentage = completionPercentage;
+        }
+    }
+}
