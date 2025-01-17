916b6db38e10cb6dbc735b269ceb672b900ae4de
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-17 17:38:58 +0300

    Employee Create

---
+++ HRAnalytics.Application/DTOs/Employee/CreateEmployeeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Application.DTOs.Employee
+{
+
+    public class CreateEmployeeDto
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string? PhoneNumber { get; set; }
+        public DateTime HireDate { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
--- HRAnalytics.Application/DTOs/Employee/UpdateEmployeeDto.cs
+++ HRAnalytics.Application/DTOs/Employee/UpdateEmployeeDto.cs
@@ -4,18 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HRAnalytics.Application.DTOs
+namespace HRAnalytics.Application.DTOs.Employee
 {
-    public class EmployeeDto
+    public class UpdateEmployeeDto
     {
-        public int Id { get; set; }
         public string FirstName { get; set; } = default!;
         public string LastName { get; set; } = default!;
         public string Email { get; set; } = default!;
         public string? PhoneNumber { get; set; }
-        public DateTime HireDate { get; set; }
         public int DepartmentId { get; set; }
-        public string DepartmentName { get; set; } = default!;
-
     }
 }
