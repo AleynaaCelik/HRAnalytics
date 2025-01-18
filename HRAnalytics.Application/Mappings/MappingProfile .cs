f6f2078a9f25da8bdc9af14a5083c9e83cb7da8c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-18 20:45:07 +0300

    AutoMapper Profiles Added

---
+++ HRAnalytics.Application/Mappings/MappingProfile .cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HRAnalytics.Application.DTOs.Employee;
+using HRAnalytics.Application.DTOs.Progress;
+using HRAnalytics.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>()
+                .ForMember(dest => dest.DepartmentName,
+                    opt => opt.MapFrom(src => src.Department.Name));
+
+            CreateMap<CreateEmployeeDto, Employee>();
+            CreateMap<UpdateEmployeeDto, Employee>();
+
+            CreateMap<EmployeeProgress, ModuleProgressDto>()
+                .ForMember(dest => dest.ModuleName,
+                    opt => opt.MapFrom(src => src.Module.Name));
+
+            CreateMap<Department, DepartmentDto>();
+            CreateMap<LearningModule, LearningModuleDto>();
+        }
+    }
+}
