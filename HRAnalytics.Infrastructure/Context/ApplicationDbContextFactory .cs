10962fd1f0ff60dd357cd50d74f0414bbf81bfad
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-22 23:48:59 +0300

    Migration Created

---
+++ HRAnalytics.Infrastructure/Context/ApplicationDbContextFactory .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Infrastructure.Context
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=HRAnalyticsDb;User Id=sa;Password=Hr@Analytics123;TrustServerCertificate=True;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
