0901e7091e55b9608cb7972b838493f4762c7a8d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-23 23:21:25 +0300

    User Entity Created

---
+++ HRAnalytics.Core/Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Core.Entities
+{
+    public class Role
+    {
+        public const string Admin = "Admin";
+        public const string Manager = "Manager";
+        public const string Employee = "Employee";
+    }
+}
---
+++ HRAnalytics.Core/Entities/User .cs
@@ -0,0 +1,19 @@
+﻿using HRAnalytics.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiry { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
