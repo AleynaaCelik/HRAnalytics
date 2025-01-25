dd4225adb5e235048f1cdb4bd92ede2b84e5114b
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-25 22:21:00 +0300

    AuthValidators Added

---
+++ HRAnalytics.API/Validators/AuthLoginRequestValidator.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+using HRAnalytics.API.Models.Requests;
+
+namespace HRAnalytics.API.Validators
+{
+    public class AuthLoginRequestValidator : AbstractValidator<AuthLoginRequest>
+    {
+        public AuthLoginRequestValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(50);
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(6);
+        }
+    }
+
+    public class AuthRefreshTokenRequestValidator : AbstractValidator<AuthRefreshTokenRequest>
+    {
+        public AuthRefreshTokenRequestValidator()
+        {
+            RuleFor(x => x.AccessToken).NotEmpty();
+            RuleFor(x => x.RefreshToken).NotEmpty();
+        }
+    }
+
+    public class AuthRegisterRequestValidator : AbstractValidator<AuthRegisterRequest>
+    {
+        public AuthRegisterRequestValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(50);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(6)
+                .Matches("[A-Z]").WithMessage("Şifre en az bir büyük harf içermelidir.")
+                .Matches("[0-9]").WithMessage("Şifre en az bir rakam içermelidir.");
+        }
+    }
+
+}
+
