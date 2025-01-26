dbfbc91f97f82e3bed707b8f52f01fa793c1415c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-26 23:52:59 +0300

    ApiException Added

---
+++ HRAnalytics.API/Response/ApiException.cs
@@ -0,0 +1,10 @@
+﻿namespace HRAnalytics.API.Response
+{
+    public class ApiException
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+        public string TraceId { get; set; }
+    }
+}
