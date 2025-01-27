a7f9437fa1da0aa8067b9d886f3838cf63bd0487
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-27 22:48:57 +0300

    global exception handling middleware updated

---
+++ HRAnalytics.API/Response/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace HRAnalytics.API.Response
+{
+    public class ApiResponse<T>
+    {
+        public T Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        public static ApiResponse<T> SuccessResult(T data, string message = null) =>
+            new() { Data = data, Success = true, Message = message };
+
+        public static ApiResponse<T> FailureResult(string message, IEnumerable<string> errors = null) =>
+            new() { Success = false, Message = message, Errors = errors };
+    }
+
+}
