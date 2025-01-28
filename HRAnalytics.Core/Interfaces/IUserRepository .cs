aa3129ea1c44c5031100372c4e3a7ab23b00d185
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-28 23:45:47 +0300

    Repositories extension added

---
+++ HRAnalytics.Core/Interfaces/IUserRepository .cs
@@ -0,0 +1,15 @@
+﻿using HRAnalytics.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Core.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetUserByUsernameAsync(string username);
+        Task<bool> UsernameExistsAsync(string username);
+    }
+}
---
+++ HRAnalytics.Infrastructure/Repositories/UserRepository .cs
@@ -0,0 +1,29 @@
+﻿using HRAnalytics.Core.Entities;
+using HRAnalytics.Core.Interfaces;
+using HRAnalytics.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Infrastructure.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Set<User>()
+                .FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<bool> UsernameExistsAsync(string username)
+        {
+            return await _context.Set<User>()
+                .AnyAsync(u => u.Username == username);
+        }
+    }
+}
