3c10fa2236c082f5229e55916c4a9754818c554e
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-01-30 23:22:30 +0300

    Authorization for Constants  created

---
+++ HRAnalytics.Core/Constants/Roles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAnalytics.Core.Constants
+{
+   
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Manager = "Manager";
+        public const string Employee = "Employee";
+
+        public static readonly IReadOnlyList<string> AllRoles = new[]
+        {
+            Admin,
+            Manager,
+            Employee
+        };
+    }
+}
+
+
+
