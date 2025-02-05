9c42162b148f7a12f887fd544e8c25b06149aae7
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-02-06 00:02:00 +0300

    AuthLoginRequestValidator and AuthRegisterRequestValidator Updated

--- HRAnalytics.Application/Validators/Auth/AuthRegisterRequestValidator .cs
+++ HRAnalytics.Application/Validators/Auth/AuthRegisterRequestValidator .cs
@@ -8,21 +8,25 @@ using System.Threading.Tasks;
 
 namespace HRAnalytics.Application.Validators.Auth
 {
-    public class AuthLoginRequestValidator : AbstractValidator<LoginRequest>
+    public class AuthRegisterRequestValidator : AbstractValidator<RegisterRequest>
     {
-        public AuthLoginRequestValidator()
+        public AuthRegisterRequestValidator()
         {
             RuleFor(x => x.Username)
                 .NotEmpty().WithMessage("Kullanıcı adı zorunludur")
                 .MinimumLength(3).WithMessage("Kullanıcı adı en az 3 karakter olmalıdır")
                 .MaximumLength(50).WithMessage("Kullanıcı adı en fazla 50 karakter olabilir");
 
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email zorunludur")
+                .EmailAddress().WithMessage("Geçerli bir email adresi giriniz");
+
             RuleFor(x => x.Password)
                 .NotEmpty().WithMessage("Şifre zorunludur")
-                .MinimumLength(6).WithMessage("Şifre en az 6 karakter olmalıdır");
+                .MinimumLength(6).WithMessage("Şifre en az 6 karakter olmalıdır")
+                .Matches("[A-Z]").WithMessage("Şifre en az bir büyük harf içermelidir")
+                .Matches("[0-9]").WithMessage("Şifre en az bir rakam içermelidir")
+                .Matches("[^a-zA-Z0-9]").WithMessage("Şifre en az bir özel karakter içermelidir");
         }
     }
-
-   
-
 }
