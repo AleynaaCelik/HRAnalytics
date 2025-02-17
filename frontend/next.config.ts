5ff7dd14b9592f316be5b36b4bd9f8c53563f7c1
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-02-17 23:17:31 +0300

    next.config.ts updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://localhost:7264/api/:path*' // API'nizin çalıştığı port
+      }
+    ]
+  }
 };
 
 export default nextConfig;
